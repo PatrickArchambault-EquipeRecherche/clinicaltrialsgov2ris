999bed27463d80b9d8898c39f15804820c594c54
PatrickArchambault-EquipeRecherche <83590296+PatrickArchambault-EquipeRecherche@users.noreply.github.com>
2024-03-13 16:11:54 -0400

    Updating interface to allow default output, and input as a list.

--- ctg2ris.py
+++ ctg2ris.py
@@ -16,7 +16,38 @@ def no_newline(my_string):
 
     return new_str
 
-def convertCSVtoRIS(csv_filename, ris_filename):
+
+def convertListtoRIS(listname):
+    my_csv = []
+    reader = csv.reader(listname)
+    header = next(reader)
+    header = header # This is just to quiet the linter
+    #print(header)
+    for row in reader:
+        pruned_row = [row[0],row[1],row[2],row[4],row[5],row[8],row[20],row[24],row[29]]
+        my_csv.append(pruned_row)
+    
+    ris_tags = ['DOI - ','TI - ','UR - ','J2 - ','AB - ','U1 - ','U2 - ','YR - ','L1 - ']
+    
+    whole_file = ""
+
+    today = datetime.datetime.strftime(datetime.datetime.now(), "%Y/%m/%d")
+
+    for trial in my_csv:
+        ris_str = "TY  - JOUR\n"
+        tags_and_values = zip(ris_tags, trial)
+        for pair in tags_and_values:
+            ris_str += f"{pair[0]}{no_newline(pair[1])}\n"
+        ris_str += "DB - clinicaltrials.gov\n"
+        ris_str += f"RD - {today}\n"
+        ris_str += "ER - \n"
+        #print(ris_str)
+        whole_file += ris_str
+    
+    return whole_file
+
+
+def convertCSVtoRIS(csv_filename, ris_filename="example.ris"):
     my_csv = []
     with open(csv_filename, encoding="utf8") as csvfile:
         reader = csv.reader(csvfile)
@@ -26,7 +57,9 @@ def convertCSVtoRIS(csv_filename, ris_filename):
         for row in reader:
             pruned_row = [row[0],row[1],row[2],row[4],row[5],row[8],row[20],row[24],row[29]]
             my_csv.append(pruned_row)
+    
     ris_tags = ['DOI - ','TI - ','UR - ','J2 - ','AB - ','U1 - ','U2 - ','YR - ','L1 - ']
+    
     whole_file = ""
 
     today = datetime.datetime.strftime(datetime.datetime.now(), "%Y/%m/%d")
@@ -52,9 +85,23 @@ def convertCSVtoRIS(csv_filename, ris_filename):
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("USAGE: ctg2ris.py CSVFILENAME RISFILENAME")
+        print("Text output only.")
+        with open("ctg-studies.csv", encoding="utf8") as fh:
+            file_as_list = []
+            for row in fh:
+                file_as_list.append(row)
+            finished = convertListtoRIS(file_as_list)
+        print(finished)
+    elif len(sys.argv) == 2:
+        print("File output.")
+        finished = convertCSVtoRIS(sys.argv[1])
+        print(finished)
     elif len(sys.argv) == 3:
+        print("File output.")
         finished = convertCSVtoRIS(sys.argv[1], sys.argv[2])
         print(finished)
     else:
         print("Wrong number of arguments.\nUSAGE: ctg2ris.py CSVFILENAME RISFILENAME")
+        print("Arguments:\n")
+        for index, arg in enumerate(sys.argv):
+            print(f"{index}: {arg}")
